bff1b839cac3fa8c6a58df75f1ef791a582f5e17
CBeenham <conor.beenham@ucdconnect.ie>
2018-01-31 12:24:08 +0000

    Unused file

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import com.Team11.Cluedo.UI.GameScreen;
-
-import java.io.IOException;
-
-public class Main {
-    public static void main(String[] args) throws IOException{
-        GameScreen gameScreen = new GameScreen();
-    }
-}
